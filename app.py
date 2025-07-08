99a2d0744bb471590aeb39ea13b7842ee9497982
Ali Rahamtalla Ali Babiker <61141558+AliRahma@users.noreply.github.com>
2025-07-08 15:06:40 +0400

    update

--- app.py
+++ app.py
@@ -2487,7 +2487,7 @@ else:
 st.markdown("---")
 st.markdown(
     """<div style="text-align:center; color:#888; font-size:0.8em;">
-    Intellipen SmartQ Test V3.6 | Developed by Ali Babiker | © June 2025
+    Intellipen SmartQ Test V4.0 | Developed by Ali Babiker | © July 2025
     </div>""",
     unsafe_allow_html=True
 )
