c9e867d4fc19a91284bcf9426d6acdf63479cb7a
Ali Rahamtalla Ali Babiker <61141558+AliRahma@users.noreply.github.com>
2025-07-15 08:40:54 +0400

    update

--- app.py
+++ app.py
@@ -966,7 +966,7 @@ else:
                     # Total row
                     total_row = {
                         'Status': 'Total',
-                        'All Count': merged_status['All Count'].sum(),
+                        'Cases Count': merged_status['All Count'].sum(),
                         'Unique Count': merged_status['Unique Count'].sum()
                     }
                     
